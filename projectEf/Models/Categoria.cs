504271c72f982ee2da39222b53f5507c5186ca78
juansearg <juansviazzo@gmail.com>
2023-02-24 16:27:28 -0300

    Actualizado

---
+++ projectEf/Models/Categoria.cs
@@ -0,0 +1,20 @@
+namespace proyectoEf.Models
+{
+
+
+
+public class Categoria
+{
+    public Guid CategoriaId {get;set;}
+    public string? Nombre {get;set;}
+    public string? Descripcion {get;set;}
+    public virtual ICollection<Tarea>? Tareas {get;set;}
+
+
+}
+
+
+
+
+    
+}
